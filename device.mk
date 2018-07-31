e12ce367313da2235d4d2f107f6be2a40702f5a4
Marc Bourgoin <themard85@gmail.com>
2018-07-31 16:29:47 +0200

    op3: Copy qti_whitelist.xml to the proper target folder
    Change-Id: I713b3f66a166a0c0dccb18091942180ede45a73e
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -375,6 +375,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
